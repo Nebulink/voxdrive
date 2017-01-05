bf5ca58082963fcdcf9ca219bd6cfcf7111e459b
denissellu <hingasellu@gmail.com>
2017-01-05 00:14:56 +0000

    add three.js and a thing

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow} = require('electron')
 
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  app.quit();
+});
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(`file://${__dirname}/index.html`)
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden'
+  })
+
+  win.loadURL(`file://${__dirname}/app/index.html`)
+  win.webContents.openDevTools();
 })
